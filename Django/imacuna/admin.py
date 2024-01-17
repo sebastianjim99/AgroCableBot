1f40e560ceca6544f8dfa346ce39ea314b1bb2d7
juan sebastian jimenez peña <2420192018@estudiantesunibague.edu.co>
2024-01-17 10:56:06 -0500

    confirmar

--- Django/imacuna/admin.py
+++ Django/imacuna/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Usuarios, Lineas_investigacion
+from .models import Usuarios, Lineas_investigacion, Servicios
 
 admin.site.register(Usuarios)
 admin.site.register(Lineas_investigacion)
+admin.site.register(Servicios)
