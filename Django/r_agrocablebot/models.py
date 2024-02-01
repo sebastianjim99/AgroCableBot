496236a84c765f61273bd75bc675308ac7d1ef8f
jf13bl <2420191018@estudiantesunibague.edu.co>
2024-02-01 11:56:23 -0500

    Actualizacion modelos AgroCableBot

--- Django/r_agrocablebot/models.py
+++ Django/r_agrocablebot/models.py
@@ -62,14 +62,33 @@ class tipoCultivo(models.Model):
         db_table = 'tipos_cultivos'
         ordering = ['id']
 
+class sensor(models.Model):
+    valorDecimal=models.DecimalField( max_digits=3,decimal_places=3,blank=True,verbose_name='Valor del sensor decimal')
+    #valorSTR=models.CharField(max_length=50, verbose_name='Valor del sensor String')
+    coordenadaX=models.DecimalField(max_digits=3,decimal_places=3,blank=True,verbose_name='Coordenada en el eje X')
+    coordenadaY=models.DecimalField(max_digits=3,decimal_places=3,blank=True,verbose_name='Coordenada en el eje Y')
+    coordenadaZ=models.DecimalField(max_digits=3,decimal_places=3,blank=True,verbose_name='Coordenada en el eje Z')
+    fecha_creado = models.DateTimeField(auto_now_add=True)
+    tipoSensor=models.ForeignKey(tipoSensor, null=True,blank=True,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Sensor'
+        verbose_name_plural = 'Sensores'
+        db_table = 'sensores'
+        ordering = ['id']
+
 
 class cultivo(models.Model):
     nombre=models.CharField(max_length= 100, verbose_name='Nombre')
     cantidad= models.PositiveSmallIntegerField(blank=True,verbose_name='Cantidad de plantas')
-    tipoCultivo=models.ForeignKey(tipoCultivo, null=True,blank=True,on_delete=models.CASCADE)
     responable= models.CharField(max_length=100)
-    correo=models.CharField(max_length= 80, verbose_name='Correo Electrónico')
+    correo=models.CharField(max_length= 100, verbose_name='Correo Electrónico')
     fechaSiembra = models.DateField(verbose_name='Fecha de siembra')
+    tipoCultivo=models.ForeignKey(tipoCultivo, null=True,blank=True,on_delete=models.CASCADE)
+    sensores=models.ManyToManyField(sensor, verbose_name="Sensores",blank=True)
     
     def __str__(self):
         return self.nombre
@@ -96,30 +115,10 @@ class plantas(models.Model):
         db_table = 'plantas'
         ordering = ['id']
 
-class sensor(models.Model):
-    valorDecimal=models.DecimalField( max_digits=3,decimal_places=3,blank=True,verbose_name='Valor del sensor decimal')
-    #valorSTR=models.CharField(max_length=50, verbose_name='Valor del sensor String')
-    coordenadaX=models.DecimalField(max_digits=3,decimal_places=3,blank=True,verbose_name='Coordenada en el eje X')
-    coordenadaY=models.DecimalField(max_digits=3,decimal_places=3,blank=True,verbose_name='Coordenada en el eje Y')
-    coordenadaZ=models.DecimalField(max_digits=3,decimal_places=3,blank=True,verbose_name='Coordenada en el eje Z')
-    fecha_creado = models.DateTimeField(auto_now_add=True)
-    fecha_actualizado = models.DateTimeField(auto_now=True)
-    tipoSensor=models.ForeignKey(tipoSensor, null=True,blank=True,on_delete=models.CASCADE)
-    ###cultivo=models.ForeignKey(cultivo, null=True,blank=True,on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.nombre
-
-    class Meta:
-        verbose_name = 'Sensor'
-        verbose_name_plural = 'Sensores'
-        db_table = 'sensores'
-        ordering = ['id']
-
 
 
 class imagenesxPlanta(models.Model):
-    imagen = models.ImageField(upload_to='iconos/plantas/', verbose_name="imagen", null= True )
+    imagen = models.ImageField(upload_to='iconos/plantas/', verbose_name="imagen", null= True ,blank=True)
     descripcion=models.TextField(blank=True)
     plantas=models.ForeignKey(plantas, null=True,blank=True,on_delete=models.CASCADE)
     
@@ -132,48 +131,48 @@ class imagenesxPlanta(models.Model):
         db_table = 'imagenes_plantas'
         ordering = ['id']
 
-class tablasEstadisticas(models.Model):
-    nombre=models.CharField(max_length= 100, verbose_name='Nombre', unique=True)
-    descripcion=models.TextField(blank=True)
-    sensor=models.ForeignKey(sensor, null=True,blank=True,on_delete=models.CASCADE)
+# class tablasEstadisticas(models.Model):
+#     nombre=models.CharField(max_length= 100, verbose_name='Nombre', unique=True)
+#     descripcion=models.TextField(blank=True)
+#     sensor=models.ForeignKey(sensor, null=True,blank=True,on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.nombre
+#     def __str__(self):
+#         return self.nombre
 
-    class Meta:
-        verbose_name = 'Tabla de estadísticas'
-        verbose_name_plural = 'Tablas de estadísticas'
-        db_table = 'tablas_estadisticas'
-        ordering = ['id']
+#     class Meta:
+#         verbose_name = 'Tabla de estadísticas'
+#         verbose_name_plural = 'Tablas de estadísticas'
+#         db_table = 'tablas_estadisticas'
+#         ordering = ['id']
 
-class graficos(models.Model):
-    nombre=models.CharField(max_length= 100, verbose_name='Nombre', unique=True)
-    descripcion=models.TextField(blank=True)
-    sensor=models.ForeignKey(sensor, null=True,blank=True,on_delete=models.CASCADE)
+# class graficos(models.Model):
+#     nombre=models.CharField(max_length= 100, verbose_name='Nombre', unique=True)
+#     descripcion=models.TextField(blank=True)
+#     sensor=models.ForeignKey(sensor, null=True,blank=True,on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.nombre
+#     def __str__(self):
+#         return self.nombre
 
-    class Meta:
-        verbose_name = 'Gráfico'
-        verbose_name_plural = 'Gráficos'
-        db_table = 'graficos'
-        ordering = ['id']
+#     class Meta:
+#         verbose_name = 'Gráfico'
+#         verbose_name_plural = 'Gráficos'
+#         db_table = 'graficos'
+#         ordering = ['id']
 
 
-class estadisticas(models.Model):
-    nombre=models.CharField(max_length= 100, verbose_name='Nombre', unique=True)
-    tablasEstadisticas=models.ForeignKey(tablasEstadisticas, null=True,blank=True,on_delete=models.CASCADE)
-    graficos=models.ForeignKey(graficos, null=True,blank=True,on_delete=models.CASCADE)
+# class estadisticas(models.Model):
+#     nombre=models.CharField(max_length= 100, verbose_name='Nombre', unique=True)
+#     tablasEstadisticas=models.ForeignKey(tablasEstadisticas, null=True,blank=True,on_delete=models.CASCADE)
+#     graficos=models.ForeignKey(graficos, null=True,blank=True,on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.nombre
+#     def __str__(self):
+#         return self.nombre
 
-    class Meta:
-        verbose_name = 'Estadística'
-        verbose_name_plural = 'Estadísticas'
-        db_table = 'estadisticas'
-        ordering = ['id']
+#     class Meta:
+#         verbose_name = 'Estadística'
+#         verbose_name_plural = 'Estadísticas'
+#         db_table = 'estadisticas'
+#         ordering = ['id']
 
 class calendarios(models.Model):
     nombre=models.CharField(max_length= 100, verbose_name='Nombre', unique=True)
@@ -183,7 +182,7 @@ class calendarios(models.Model):
     hora=models.TimeField()
     cultivo=models.ForeignKey(cultivo, null=True,blank=True,on_delete=models.CASCADE)
     plantas=models.ForeignKey(plantas, null=True,blank=True,on_delete=models.CASCADE)
-    estadisticas=models.ForeignKey(estadisticas, null=True,blank=True,on_delete=models.CASCADE)
+    
 
 
     def __str__(self):
