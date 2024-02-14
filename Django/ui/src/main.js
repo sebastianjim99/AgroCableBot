f23ea41eb409be476b6256aa146c0258c0cbce97
juan sebastian jimenez peña <2420192018@estudiantesunibague.edu.co>
2024-02-14 10:37:42 -0500

    estilos de integrantes

--- Django/ui/src/main.js
+++ Django/ui/src/main.js
@@ -2,12 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import '../src/resources/css/bootstrap.css'
+import '../src/assets/vendor/swiper/swiper-bundle.min.css'
+// Import Swiper styles
+  import 'swiper/css';
+  import 'swiper/css/pagination';
 import axios from 'axios'
 import store from './store'
 import { BootstrapIconsPlugin } from "bootstrap-icons-vue";
 import '../src/resources/css/Bootstrap-Calendar.css' 
+import '../src/resources/css/integrantes.css' 
 
-axios.defaults.baseURL = 'http://localhost:8000'
+
+axios.defaults.baseURL = 'http://0.0.0.0:8000'
 
 // createApp(App).use(router).use(store).mount('#app')
 
